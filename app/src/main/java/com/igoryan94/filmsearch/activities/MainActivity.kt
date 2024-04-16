08e1cb705bf6b98d658f00996b248fcd78ec0222
Igor K. <igoryan94@gmail.com>
2024-04-16 19:25:15 +0300

    improve main screen via implementing some UI details

--- app/src/main/java/com/igoryan94/filmsearch/activities/MainActivity.kt
+++ app/src/main/java/com/igoryan94/filmsearch/activities/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.igoryan94.filmsearch.activities
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.dragonfly.tweaks.toast
 import com.igoryan94.filmsearch.R
 import com.igoryan94.filmsearch.databinding.ActivityMainBinding
 
@@ -23,8 +24,20 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        b.contents.setOnClickListener {
-            startActivity(Intent(this, ImageViewTestActivity::class.java))
-        }
+//        b.contents.setOnClickListener {
+//            startActivity(Intent(this, ImageViewTestActivity::class.java))
+//        }
+        setupButtons()
+    }
+
+    private fun setupButtons() {
+        for (button in listOf(
+            b.buttonMenu,
+            b.buttonFavorites,
+            b.buttonRecommendations,
+            b.buttonWatchLater,
+            b.buttonSettings
+        ))
+            button.setOnClickListener { "${(it as Button).text}".toast(this) }
     }
 }
