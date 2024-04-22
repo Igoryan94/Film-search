4b3173ce1856fc12f25980fc33920703c39419e8
Igor K. <igoryan94@gmail.com>
2024-04-22 08:58:21 +0300

    activity_main: add custom subscribe view

---
+++ app/src/main/java/com/igoryan94/filmsearch/views/SubscribeView.kt
@@ -0,0 +1,31 @@
+package com.igoryan94.filmsearch.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
+import com.igoryan94.filmsearch.R
+
+class SubscribeView(context: Context, attributeSet: AttributeSet) :
+    LinearLayout(context, attributeSet) {
+    private val editText: EditText
+    private val subscribeButton: Button
+
+    init {
+        // "Надуваем наш фрагмент"
+        LayoutInflater.from(context).inflate(R.layout.subscribe, this)
+        //Нужно явно указать ориентацию, если она вертикальная, иначе отображение будет не корректное
+        this.orientation = VERTICAL
+        // Привязываем наши View из xml (можно и без переменных обращаться к ним сразу напрямую)
+        editText = findViewById(R.id.et_subscribe)
+        subscribeButton = findViewById(R.id.btn_subscribe)
+
+        //Вешаем слушатель на нашу кнопку, сейчас он отправляет данные из EditText в Toast, в рабочем приложении он бы отправлял данные на сервер
+        subscribeButton.setOnClickListener {
+            Toast.makeText(context, editText.text, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
