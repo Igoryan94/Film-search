4ed70bd3a2658bde8a3d50fbfcbf10012d921b15
Igor K. <igoryan94@gmail.com>
2024-04-22 11:23:17 +0300

    ImageViewTestActivity: add ViewPager2 element in the bottom of the activity

--- app/src/main/java/com/igoryan94/filmsearch/activities/ImageViewTestActivity.kt
+++ app/src/main/java/com/igoryan94/filmsearch/activities/ImageViewTestActivity.kt
@@ -1,15 +1,25 @@
 package com.igoryan94.filmsearch.activities
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.igoryan94.filmsearch.R
 import com.igoryan94.filmsearch.databinding.ActivityImageViewTestBinding
 
 class ImageViewTestActivity : AppCompatActivity() {
-    private lateinit var b: ActivityImageViewTestBinding
+    lateinit var b: ActivityImageViewTestBinding
+    private lateinit var pagerAdapter: MyAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,5 +30,63 @@ class ImageViewTestActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //Создаем адаптер
+        pagerAdapter = MyAdapter()
+
+        //Привязываем созданный адаптер к нашему ViewPager, который у нас в разметке
+        b.viewPager2.apply {
+            orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            adapter = pagerAdapter
+
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+
+            })
+        }
+
+        //Создаем список элементов, который передадим в адаптер
+        val pagerItems = listOf(
+            PagerItem(ContextCompat.getColor(this, R.color.colorAccent), "Accent"),
+            PagerItem(ContextCompat.getColor(this, R.color.colorCat), "Cat"),
+            PagerItem(ContextCompat.getColor(this, R.color.backgroundColor), "BG")
+        )
+
+        //Передаем список в адаптер
+        pagerAdapter.setItems(pagerItems)
+    }
+
+    class PagerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        //В этом методе мы передаем данные из PagerItem в нашу верстку item.xml
+        fun onBind(item: PagerItem) {
+            //корневой элемент item.xml
+            (itemView as? ConstraintLayout)?.apply {
+                setBackgroundColor(item.color)
+                findViewById<TextView>(R.id.textView).text = item.text
+            }
+        }
+    }
+
+    data class PagerItem(val color: Int, val text: String)
+
+    // Адаптер для ViewPager2
+    class MyAdapter : RecyclerView.Adapter<PagerViewHolder>() {
+        private var items = mutableListOf<PagerItem>()
+
+        fun setItems(newItems: List<PagerItem>) {
+            items.clear()
+            items.addAll(newItems)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagerViewHolder =
+            PagerViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.tabs_item, parent, false)
+            )
+
+        override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
+            holder.onBind(items[position])
+        }
+
+        override fun getItemCount(): Int = items.size
     }
 }
--- app/src/main/java/com/igoryan94/filmsearch/views/SubscribeView.kt
+++ app/src/main/java/com/igoryan94/filmsearch/views/SubscribeView.kt
@@ -1,6 +1,7 @@
 package com.igoryan94.filmsearch.views
 
 import android.content.Context
+import android.content.Intent
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.Button
@@ -8,6 +9,7 @@ import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.Toast
 import com.igoryan94.filmsearch.R
+import com.igoryan94.filmsearch.activities.ImageViewTestActivity
 
 class SubscribeView(context: Context, attributeSet: AttributeSet) :
     LinearLayout(context, attributeSet) {
@@ -26,6 +28,8 @@ class SubscribeView(context: Context, attributeSet: AttributeSet) :
         //Вешаем слушатель на нашу кнопку, сейчас он отправляет данные из EditText в Toast, в рабочем приложении он бы отправлял данные на сервер
         subscribeButton.setOnClickListener {
             Toast.makeText(context, editText.text, Toast.LENGTH_SHORT).show()
+
+            context.startActivity(Intent(context, ImageViewTestActivity::class.java))
         }
     }
 }
