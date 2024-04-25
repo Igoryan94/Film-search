8df5f95781c6bc438941386f1f7a897b48e6ca35
Igor K. <igoryan94@gmail.com>
2024-04-25 12:32:20 +0300

    AnimSetActivity: add various interpolators to moving objects

--- app/src/main/java/com/igoryan94/filmsearch/activities/AnimSetActivity.kt
+++ app/src/main/java/com/igoryan94/filmsearch/activities/AnimSetActivity.kt
@@ -1,9 +1,13 @@
 package com.igoryan94.filmsearch.activities
 
+import android.animation.Animator
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AnticipateInterpolator
+import android.view.animation.OvershootInterpolator
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -26,6 +30,7 @@ class AnimSetActivity : AppCompatActivity() {
 
         val sunRiseAnim = ObjectAnimator.ofFloat(b.imageSun, View.TRANSLATION_Y, -700f)
         sunRiseAnim.duration = 1500
+        sunRiseAnim.interpolator = AccelerateDecelerateInterpolator()
 
         val sunAppearAnim = ObjectAnimator.ofFloat(b.imageSun, View.ALPHA, 1f)
         sunAppearAnim.duration = 300
@@ -35,12 +40,14 @@ class AnimSetActivity : AppCompatActivity() {
 
         val firstCloudAnim = ObjectAnimator.ofFloat(b.imageCloud1, View.TRANSLATION_X, 0F)
         firstCloudAnim.duration = 1000
+        firstCloudAnim.interpolator = OvershootInterpolator()
 
         val firstCloudAppearAnim = ObjectAnimator.ofFloat(b.imageCloud1, View.ALPHA, 1f)
         firstCloudAppearAnim.duration = 300
 
         val secondCloudAnim = ObjectAnimator.ofFloat(b.imageCloud2, View.TRANSLATION_X, 0F)
         secondCloudAnim.duration = 1000
+        secondCloudAnim.interpolator = OvershootInterpolator()
 
         val secondCloudAppearAnim = ObjectAnimator.ofFloat(b.imageCloud2, View.ALPHA, 1f)
         secondCloudAppearAnim.duration = 300
@@ -51,16 +58,38 @@ class AnimSetActivity : AppCompatActivity() {
         val cloud2OffScreenAnim = ObjectAnimator.ofFloat(b.imageCloud2, View.TRANSLATION_X, 500f)
         cloud2OffScreenAnim.duration = 1000
 
-        val animatorSun = AnimatorSet()
-        animatorSun.playTogether(sunAppearAnim, sunRiseAnim, nightToDayAnim)
-        animatorSun.play(firstCloudAppearAnim).after(sunRiseAnim)
-        animatorSun.play(firstCloudAnim).after(firstCloudAppearAnim)
-        animatorSun.play(secondCloudAppearAnim).after(firstCloudAnim).after(500)
-        animatorSun.play(secondCloudAnim).after(secondCloudAppearAnim)
+        val animator = AnimatorSet()
+        animator.playTogether(sunAppearAnim, sunRiseAnim, nightToDayAnim)
+        animator.play(firstCloudAppearAnim).after(sunRiseAnim)
+        animator.play(firstCloudAnim).after(firstCloudAppearAnim)
+        animator.play(secondCloudAppearAnim).after(firstCloudAnim).after(500)
+        animator.play(secondCloudAnim).after(secondCloudAppearAnim)
 
-        animatorSun.play(cloud1OffScreenAnim).before(firstCloudAnim)
-        animatorSun.play(cloud2OffScreenAnim).before(secondCloudAnim)
+        animator.play(cloud1OffScreenAnim).before(firstCloudAnim)
+        animator.play(cloud2OffScreenAnim).before(secondCloudAnim)
 
-        animatorSun.start()
+        animator.start()
+
+        b.imageSun.setOnClickListener {
+            b.imageSun.animate()
+                .setDuration(300)
+                .setInterpolator(AnticipateInterpolator())
+                .alpha(0f)
+                .setListener(object : Animator.AnimatorListener {
+                    override fun onAnimationStart(animation: Animator) {
+                    }
+
+                    override fun onAnimationEnd(animation: Animator) {
+                    }
+
+                    override fun onAnimationCancel(animation: Animator) {
+                    }
+
+                    override fun onAnimationRepeat(animation: Animator) {
+                    }
+
+                })
+                .start()
+        }
     }
 }
