394533d5c4f643b832800e6f3b7d6efb1278d852
Igor K. <igoryan94@gmail.com>
2024-04-25 09:13:38 +0300

    complete the task 22.3.1

--- app/src/main/java/com/igoryan94/filmsearch/activities/AnimThrowActivity.kt
+++ app/src/main/java/com/igoryan94/filmsearch/activities/AnimThrowActivity.kt
@@ -1,7 +1,10 @@
 package com.igoryan94.filmsearch.activities
 
+import android.animation.Animator
+import android.animation.ObjectAnimator
 import android.os.Bundle
-import android.view.animation.AnimationUtils
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -25,17 +28,65 @@ class AnimThrowActivity : AppCompatActivity() {
 
         mapButtons()
 
-        throwTheCar()
+//        throwTheCar()
     }
 
     private fun mapButtons() {
         b.throwingImage.setOnClickListener {
-            throwTheCar()
+//            throwTheCar()
+            throwTheCarCompletely()
         }
     }
 
-    private fun throwTheCar() {
-        val throwingAnim = AnimationUtils.loadAnimation(this, R.anim.car_throwing)
-        b.throwingImage.startAnimation(throwingAnim)
+//    private fun throwTheCar() {
+//        val throwingAnim = AnimationUtils.loadAnimation(this, R.anim.car_throwing)
+//        b.throwingImage.startAnimation(throwingAnim)
+//    }
+
+    private fun throwTheCarCompletely() {
+//        val animator = ValueAnimator.ofFloat(1f, 0f)
+//
+//        animator.addUpdateListener {
+//            view.alpha = it.animatedValue as Float
+//        }
+//        animator.start()
+
+        val animationUpdateListener = object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+                Toast.makeText(this@AnimThrowActivity, "Animation start", Toast.LENGTH_SHORT).show()
+                println("start")
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                Toast.makeText(this@AnimThrowActivity, "Animation End", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+                Toast.makeText(this@AnimThrowActivity, "Animation cancel", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+                Toast.makeText(this@AnimThrowActivity, "Animation repeat", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+        b.throwingImage.setOnClickListener {
+            val anim = ObjectAnimator.ofFloat(b.throwingImage, View.TRANSLATION_Y, 0F, -3000F)
+            anim.duration = 3000
+            anim.addListener(animationUpdateListener)
+            anim.start()
+
+            val anim1 = ObjectAnimator.ofFloat(b.throwingImage, View.TRANSLATION_X, 0F, 700F)
+            anim1.duration = 3000
+            anim1.addListener(animationUpdateListener)
+            anim1.start()
+
+            val anim2 = ObjectAnimator.ofFloat(b.throwingImage, View.ROTATION, 0F, 40F)
+            anim2.duration = 3000
+            anim2.addListener(animationUpdateListener)
+            anim2.start()
+        }
     }
 }
