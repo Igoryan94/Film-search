92365105a42ae759fb662cd146bba2149f3bd45f
Igor K. <igoryan94@gmail.com>
2024-05-04 00:34:11 +0300

    ListActivity: save/restore scroll state on rotation

--- app/src/main/java/com/igoryan94/filmsearch/activities/training/ListActivity.kt
+++ app/src/main/java/com/igoryan94/filmsearch/activities/training/ListActivity.kt
@@ -1,11 +1,15 @@
 package com.igoryan94.filmsearch.activities.training
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.animation.AnimationUtils
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.igoryan94.filmsearch.R
 import com.igoryan94.filmsearch.databinding.ActivityListBinding
 import com.igoryan94.filmsearch.views.recycler.adapters.MyAdapter
@@ -13,6 +17,11 @@ import com.igoryan94.filmsearch.views.recycler.etc.MyItemDecoration
 
 class ListActivity : AppCompatActivity() {
     private lateinit var b: ActivityListBinding
+
+    private lateinit
+    var layoutManager: RecyclerView.LayoutManager
+    private val KEY_MANAGER_STATE: String = "KeyForLayoutManagerState"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,6 +32,32 @@ class ListActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var isLoading = false
+        val scrollListener = object : RecyclerView.OnScrollListener() {
+            @Override
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val layoutManager = recyclerView.layoutManager as RecyclerView.LayoutManager
+                //смотрим сколько элементов на экране
+                val visibleItemCount: Int = layoutManager.childCount
+                //сколько всего элементов
+                val totalItemCount: Int = layoutManager.itemCount
+
+                //какая позиция первого элемента
+                val firstVisibleItems =
+                    (recyclerView.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+
+                //проверяем, грузим мы что-то или нет
+                if (!isLoading) {
+                    if (visibleItemCount + firstVisibleItems >= totalItemCount) {
+                        //ставим флаг, что мы попросили еще элементы
+                        isLoading = true
+                        //Вызывает загрузку данных в RecyclerView
+                    }
+                }
+            }
+        }
+        b.recyclerView.addOnScrollListener(scrollListener)
 
         val adapter = MyAdapter()
         b.recyclerView.apply {
@@ -41,5 +76,19 @@ class ListActivity : AppCompatActivity() {
             // Подключаем декоратор элементов
             addItemDecoration(MyItemDecoration())
         }
+
+        layoutManager = b.recyclerView.layoutManager!!
+        @SuppressLint("NewApi")
+        fun restoreState() {
+            // FIXME устаревший метод getParcelable(String)
+            val outState: Parcelable? = savedInstanceState?.getParcelable(KEY_MANAGER_STATE)
+            layoutManager.onRestoreInstanceState(outState)
+        }
+        restoreState()
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        savedInstanceState.putParcelable(KEY_MANAGER_STATE, layoutManager.onSaveInstanceState())
     }
 }
