47784e75954526756bfcc626d5c22523aec56db3
Igor K. <igoryan94@gmail.com>
2024-05-07 15:27:26 +0300

    ScrollCoordinatorActivity.kt: play with title colors on scroll

--- app/src/main/java/com/igoryan94/filmsearch/activities/training/ScrollCoordinatorActivity.kt
+++ app/src/main/java/com/igoryan94/filmsearch/activities/training/ScrollCoordinatorActivity.kt
@@ -3,19 +3,45 @@ package com.igoryan94.filmsearch.activities.training
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.igoryan94.filmsearch.R
+import com.igoryan94.filmsearch.databinding.ActivityScrollCoordinatorBinding
+import kotlin.math.abs
 
 class ScrollCoordinatorActivity : AppCompatActivity() {
+    private lateinit var b: ActivityScrollCoordinatorBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_scroll_coordinator)
+        b = ActivityScrollCoordinatorBinding.inflate(layoutInflater)
+        setContentView(b.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        b.toolbar.title = "Film search\nScrollCoordinatorActivity"
+
+        b.appBar.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
+            if (verticalOffset == 0) {
+                b.toolbarLayout.setExpandedTitleColor(
+                    ContextCompat.getColor(
+                        this@ScrollCoordinatorActivity,
+                        R.color.backgroundColor
+                    )
+                )
+            }
+            if (abs(verticalOffset) >= appBarLayout.scrollBarSize)
+                b.toolbarLayout.setCollapsedTitleTextColor(
+                    ContextCompat.getColor(
+                        this@ScrollCoordinatorActivity,
+                        R.color.colorPrimary
+                    )
+                )
+            b.toolbarLayout.title = verticalOffset.toString()
         }
     }
 }
