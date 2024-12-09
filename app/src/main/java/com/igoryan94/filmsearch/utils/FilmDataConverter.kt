a520c7de7c42f6998a9098f0e84ec7b17ecdf1af
Igor K. <igoryan94@gmail.com>
2024-12-09 18:53:13 +0300

    FilmDataConverter.kt: shorten the convertApiListToDtoList() function

--- app/src/main/java/com/igoryan94/filmsearch/utils/FilmDataConverter.kt
+++ app/src/main/java/com/igoryan94/filmsearch/utils/FilmDataConverter.kt
@@ -4,19 +4,13 @@ import com.igoryan94.filmsearch.data.entity.TmdbFilm
 import com.igoryan94.filmsearch.view.recyclerview_adapters.Film
 
 object FilmDataConverter {
-    fun convertApiListToDtoList(list: List<TmdbFilm>?): List<Film> {
-        val result = mutableListOf<Film>()
-        list?.forEach {
-            result.add(
-                Film(
-                    title = it.title,
-                    poster = it.posterPath,
-                    description = it.overview,
-                    rating = it.voteAverage,
-                    isInFavorites = false
-                )
-            )
-        }
-        return result
-    }
+    fun convertApiListToDtoList(list: List<TmdbFilm>?) = list?.map {
+        Film(
+            title = it.title,
+            poster = it.posterPath,
+            description = it.overview,
+            rating = it.voteAverage,
+            isInFavorites = false
+        )
+    }?.toMutableList() ?: mutableListOf()
 }
