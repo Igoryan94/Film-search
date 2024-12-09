bc1dd0892d78b4cc50d622e7f3d2e536bd7ed122
Igor K. <igoryan94@gmail.com>
2024-12-09 18:01:09 +0300

    LazyDomainModule.kt: move the module into DomainModule.kt

--- app/src/main/java/com/igoryan94/filmsearch/di/modules/DomainModule.kt
+++ app/src/main/java/com/igoryan94/filmsearch/di/modules/DomainModule.kt
@@ -1,9 +1,12 @@
 package com.igoryan94.filmsearch.di.modules
 
+import android.content.Context
+import com.igoryan94.filmsearch.data.PreferenceProvider
 import com.igoryan94.filmsearch.domain.Interactor
 import com.igoryan94.filmsearch.viewmodel.HomeFragmentViewModel
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import javax.inject.Singleton
 
 @Module
@@ -16,4 +19,16 @@ abstract class DomainModule {
 
 interface InteractorProvider {
     fun getFilmsFromApi(page: Int, callback: HomeFragmentViewModel.ApiCallback)
+}
+
+@Module
+class LazyDomainModule(val context: Context) {
+    // Нам нужно контекст как-то провайдить, поэтому создаем такой метод
+    @Provides
+    fun provideContext() = context
+
+    @Singleton
+    @Provides
+    // Создаем экземпляр SharedPreferences
+    fun providePreferences(context: Context) = PreferenceProvider(context)
 }
