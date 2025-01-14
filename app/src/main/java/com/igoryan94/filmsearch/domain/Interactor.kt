cc40655f5e77572052f3e01498fb711c139994fb
Igor K. <igoryan94@gmail.com>
2025-01-14 16:33:58 +0300

    module 44 unit 8: migrate all of the async work from Coroutines to RxJava

--- app/src/main/java/com/igoryan94/filmsearch/domain/Interactor.kt
+++ app/src/main/java/com/igoryan94/filmsearch/domain/Interactor.kt
@@ -1,5 +1,6 @@
 package com.igoryan94.filmsearch.domain
 
+import android.annotation.SuppressLint
 import com.igoryan94.filmsearch.data.MainRepository
 import com.igoryan94.filmsearch.data.PreferenceProvider
 import com.igoryan94.filmsearch.data.entity.ApiKey
@@ -8,10 +9,8 @@ import com.igoryan94.filmsearch.data.entity.TmdbResultsDto
 import com.igoryan94.filmsearch.di.modules.InteractorProvider
 import com.igoryan94.filmsearch.di.modules.TmdbApiProvider
 import com.igoryan94.filmsearch.viewmodel.HomeFragmentViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -24,56 +23,53 @@ class Interactor @Inject constructor(
 ) : InteractorProvider {
     // В конструктор мы будем передавать коллбэк из вью модели, чтобы реагировать на то, когда фильмы будут получены
     // и страницу, которую нужно загрузить (это для пагинации)
-    override suspend fun getFilmsFromApi(page: Int, callback: HomeFragmentViewModel.ApiCallback) {
+    override fun getFilmsFromApi(page: Int, callback: HomeFragmentViewModel.ApiCallback) {
         val retrofitService = tmdbApiProvider.provideTmdbApi()
 
-        withContext(Dispatchers.IO) {
-            // Метод getDefaultCategoryFromPreferences() будет нам получать при каждом запросе нужный нам список фильмов
-            retrofitService.getFilms(getDefaultCategoryFromPreferences(), ApiKey.ID, "ru-RU", page)
-                .enqueue(object :
-                    Callback<TmdbResultsDto> {
-                    override fun onResponse(
-                        call: Call<TmdbResultsDto>,
-                        response: Response<TmdbResultsDto>
-                    ) {
-                        CoroutineScope(Dispatchers.IO).launch {
-                            // При успехе мы вызываем метод, передаем onSuccess и в этот коллбэк - список фильмов
-                            val filmsData = response.body()?.tmdbFilms
-                            // Конвертируем полученные данные из исходного объекта List<TmdbFilm> в
-                            // пригодный для нашей обработки List<Film>. Если ошибка в данных (null),
-                            // то возвращаем пустой MutableList<Film>
-                            val list = filmsData?.map {
-                                Film(
-                                    title = it.title,
-                                    poster = it.posterPath,
-                                    description = it.overview,
-                                    rating = it.voteAverage,
-                                    isInFavorites = false
-                                )
-                            }?.toMutableList() ?: mutableListOf()
+        // Метод getDefaultCategoryFromPreferences() будет нам получать при каждом запросе нужный нам список фильмов
+        retrofitService.getFilms(getDefaultCategoryFromPreferences(), ApiKey.ID, "ru-RU", page)
+            .enqueue(object :
+                Callback<TmdbResultsDto> {
+                @SuppressLint("CheckResult")
+                override fun onResponse(
+                    call: Call<TmdbResultsDto>,
+                    response: Response<TmdbResultsDto>
+                ) {
+                    // При успехе мы вызываем метод, передаем onSuccess и в этот коллбэк - список фильмов
+                    val filmsData = response.body()?.tmdbFilms
+                    // Конвертируем полученные данные из исходного объекта List<TmdbFilm> в
+                    // пригодный для нашей обработки List<Film>. Если ошибка в данных (null),
+                    // то возвращаем пустой MutableList<Film>
+                    val list = filmsData?.map {
+                        Film(
+                            title = it.title,
+                            poster = it.posterPath,
+                            description = it.overview,
+                            rating = it.voteAverage,
+                            isInFavorites = false
+                        )
+                    }?.toMutableList() ?: mutableListOf()
 
-                            // Очищаем БД перед обновлением данных и кладем туда фильмы
-                            repository.clearDB()
-                            repository.putToDb(films = list)
+                    // Очищаем БД перед обновлением данных и кладем туда фильмы
+                    repository.clearDB()
+                        .flatMap { repository.putToDb(films = list) }
+                        .subscribeOn(Schedulers.io())
+                        .subscribe({ callback.onSuccess() }, { callback.onFailure() })
+                }
 
-                            callback.onSuccess()
-                        }
-                    }
-
-                    override fun onFailure(call: Call<TmdbResultsDto>, t: Throwable) {
-                        // В случае провала вызываем другой метод коллбека
-                        callback.onFailure()
-                    }
-                })
-        }
+                override fun onFailure(call: Call<TmdbResultsDto>, t: Throwable) {
+                    // В случае провала вызываем другой метод коллбека
+                    callback.onFailure()
+                }
+            })
     }
 
     // Метод для получения фильмов из базы, например при сетевой ошибке
-    suspend fun getFilmsFromDB(): List<Film> =
-        withContext(Dispatchers.IO) { repository.getAllFromDB() }
+    fun getFilmsFromDB(): Observable<List<Film>> = repository.getAllFromDB()
 
     // Метод для очистки базы данных
-    suspend fun clearDB() = withContext(Dispatchers.IO) { repository.clearDB() }
+    fun clearDB(): Observable<Unit> = repository.clearDB()
+
 
     // Метод для сохранения настроек
     fun saveDefaultCategoryToPreferences(category: String) {
