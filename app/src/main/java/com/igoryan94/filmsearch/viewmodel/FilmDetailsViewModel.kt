b4879cb3ce7cfa9853ffbe5562f420cabada7b15
Igor K. <igoryan94@gmail.com>
2025-01-14 19:49:19 +0300

    module 44 unit 8: migrate the rest (FilmDetailsFragment.kt, FilmDetailsViewModel.kt, AnimCircularRevealActivity.kt) from Coroutines to RxJava

--- app/src/main/java/com/igoryan94/filmsearch/viewmodel/FilmDetailsViewModel.kt
+++ app/src/main/java/com/igoryan94/filmsearch/viewmodel/FilmDetailsViewModel.kt
@@ -3,21 +3,22 @@ package com.igoryan94.filmsearch.viewmodel
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.lifecycle.ViewModel
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import timber.log.Timber
 import java.net.URL
-import kotlin.coroutines.resume
-import kotlin.coroutines.suspendCoroutine
 
 class FilmDetailsViewModel : ViewModel() {
-    suspend fun loadWallpaper(url: String): Bitmap? = suspendCoroutine {
-        val bitmap = try {
+    fun loadWallpaper(url: String): Observable<Bitmap> {
+        return Observable.fromCallable {
             val url = URL(url)
             BitmapFactory.decodeStream(url.openConnection().getInputStream())
-        } catch (e: Exception) {
-            // Залогировать ошибку
-            Timber.e("Error loading wallpaper: ${e.message}")
-            null // Вернуть null в случае ошибки
         }
-        it.resume(bitmap)
+            .subscribeOn(Schedulers.io())
+            .onErrorReturn { e ->
+                Timber.e("Error loading wallpaper: ${e.message}")
+                //Если пришел null, мы возвращаем  just(null)
+                null
+            }
     }
 }
